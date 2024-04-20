0d68cadf51a9711e0d1fa0db17c56aea8687b3ef
Maurilio Meireles <maurilio.meireles@gmail.com>
2024-04-20 01:17:09 -0300

    feat: add route to get event with id

---
+++ src/routes/get-event.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma.js";
+
+export async function getEvent(app: FastifyInstance){
+  app.withTypeProvider<ZodTypeProvider>().get('/events/:eventId',{
+    schema:{
+      params: z.object({
+        eventId: z.string().uuid()
+      })
+    }
+  },
+  async (request, reply)=>{
+    const {eventId} = request.params
+
+    const checkForEvent = await prisma.event.findUnique({
+      where:{
+        id: eventId
+      },
+      select:{
+        title: true,
+        slug: true,
+        details: true,
+        maximumAttendees: true,
+        _count:{
+          select: {
+            attendees: true
+          }
+        }
+      },
+    })
+
+    if (checkForEvent === null) {
+      throw new Error("No event with such ID")
+    }
+
+    const { title, slug, details, maximumAttendees} = checkForEvent
+    const attendeeCount = checkForEvent._count.attendees
+
+    return reply.send({
+      title,
+      slug,
+      maximumAttendees,
+      attendeeCount,
+      details
+    })
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { fastify } from "fastify";
 import {serializerCompiler, validatorCompiler} from 'fastify-type-provider-zod'
 import { createEvent } from "./routes/create-event.js";
 import { registerForEvent } from "./routes/register-for-event.js";
+import { getEvent } from "./routes/get-event.js";
 
 const app = fastify()
 
@@ -10,6 +11,7 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
 
 app.listen({
   host: "0.0.0.0",
