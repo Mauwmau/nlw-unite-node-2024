b85efbe0d223febaee8e403c5399b03ab3953af3
Maurilio Meireles <maurilio.meireles@gmail.com>
2024-04-20 22:08:56 -0300

    refactor: add response validation for status 200 on get event attendees route

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -3,8 +3,8 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma.js";
 
-export async function getEventAttendees(app:FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get('/events/:eventId/attendees',{
+export async function getEventAttendees(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get('/events/:eventId/attendees', {
     schema: {
       params: z.object({
         eventId: z.string().uuid(),
@@ -12,11 +12,22 @@ export async function getEventAttendees(app:FastifyInstance) {
       querystring: z.object({
         query: z.string().nullish(),
         pageIndex: z.string().nullish().default('0').transform(Number),
-      })
+      }),
+      response: {
+        200: z.object({
+          total: z.number().nonnegative(),
+          attendees: z.array(z.object({
+            id: z.string(),
+            name: z.string(),
+            email: z.string().email(),
+            createdAt: z.date(),
+          }))
+        })
+      }
     }
-  },async (request,reply)=>{
-    const {eventId} = request.params
-    const {query, pageIndex} = request.query    
+  }, async (request, reply) => {
+    const { eventId } = request.params
+    const { query, pageIndex } = request.query
 
     const result = await prisma.attendee.findMany({
       where: query ? {
